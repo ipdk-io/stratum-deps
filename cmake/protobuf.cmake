20d5a3d5e4102ca579344c680750d669359038cb
Derek G Foster <derek.foster@intel.com>
2023-08-09 13:57:44 -0700

    Build improvements
    - Rename RECENT_GRPC option to RECENT_PKGS. It updates more than
      just GRPC.
    
    - Only specify the protobuf_ABSL_PROVIDER option when building
      newer versions of Protobuf.
    
    Signed-off-by: Derek G Foster <derek.foster@intel.com>

--- cmake/protobuf.cmake
+++ cmake/protobuf.cmake
@@ -4,6 +4,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+unset(_absl_provider)
 unset(_build_shared_libs)
 unset(_source_subdir)
 
@@ -11,16 +12,23 @@ GetDownloadSpec(DOWNLOAD_PROTOBUF ${PROTOBUF_GIT_URL} ${PROTOBUF_GIT_TAG})
 
 # In older versions of protobuf, the primary CMakeLists.txt file
 # is in the cmake subdirectory.
-if(NOT RECENT_GRPC)
+if(NOT RECENT_PKGS)
   set(_source_subdir SOURCE_SUBDIR cmake)
 endif()
 
 # Protobuf v23.x generates unresolved external references to
 # ThreadSafeArena::ThreadCache _thread_cache if BUILD_SHARED_LIBS=ON.
-if(RECENT_GRPC)
+if(RECENT_PKGS)
   set(_build_shared_libs off)
 else()
   set(_build_shared_libs on)
+endif()
+
+# Protobuf v23.x bundles its own copy of Abseil.
+# We suppress it in favor of our own version or the
+# version bundles with gRPC.
+if(RECENT_PKGS)
+  set(_absl_provider -Dprotobuf_ABSL_PROVIDER=package)
 endif()
 
 ExternalProject_Add(protobuf
@@ -36,7 +44,7 @@ ExternalProject_Add(protobuf
     -DCMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH}
     -DCMAKE_FIND_ROOT_PATH=${CMAKE_FIND_ROOT_PATH}
     -DBUILD_SHARED_LIBS=${_build_shared_libs}
-    -Dprotobuf_ABSL_PROVIDER=package
+    ${_absl_provider}
     -Dprotobuf_BUILD_TESTS=off
   ${_source_subdir}
   INSTALL_COMMAND
