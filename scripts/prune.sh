0b726897e71e85af4f25bf857d333fa46538da14
Derek G Foster <derek.foster@intel.com>
2023-09-21 10:37:18 -0700

    Update prune.sh script
    - Add a couple of new directories.
    
    - Combine commands into a single sorted list.
    
    Signed-off-by: Derek G Foster <derek.foster@intel.com>

--- scripts/prune.sh
+++ scripts/prune.sh
@@ -8,23 +8,14 @@
 
 SOURCE_DIR=source
 
-# duplicate packages
 rm -fr ${SOURCE_DIR}/grpc/third_party/abseil-cpp
-rm -fr ${SOURCE_DIR}/grpc/third_party/bloaty/abseil-cpp
-rm -fr ${SOURCE_DIR}/grpc/third_party/bloaty/third_party/abseil-cpp
-rm -fr ${SOURCE_DIR}/grpc/third_party/bloaty/third_party/protobuf
-rm -fr ${SOURCE_DIR}/grpc/third_party/bloaty/third_party/zlib
-rm -fr ${SOURCE_DIR}/grpc/third_party/cares
-rm -fr ${SOURCE_DIR}/grpc/third_party/protobuf
-rm -fr ${SOURCE_DIR}/grpc/third_party/zlib
-
-rm -fr ${SOURCE_DIR}/protobuf/third_party/abseil-cpp
-rm -fr ${SOURCE_DIR}/protobuf/third_party/zlib
-
-# unused packages
 rm -fr ${SOURCE_DIR}/grpc/third_party/benchmark
 rm -fr ${SOURCE_DIR}/grpc/third_party/bloaty
 rm -fr ${SOURCE_DIR}/grpc/third_party/boringssl-with-bazel
+rm -fr ${SOURCE_DIR}/grpc/third_party/cares
 rm -fr ${SOURCE_DIR}/grpc/third_party/libuv
-
+rm -fr ${SOURCE_DIR}/grpc/third_party/protobuf
+rm -fr ${SOURCE_DIR}/grpc/third_party/zlib
+rm -fr ${SOURCE_DIR}/protobuf/third_party/abseil-cpp
 rm -fr ${SOURCE_DIR}/protobuf/third_party/benchmark
+rm -fr ${SOURCE_DIR}/protobuf/third_party/zlib
