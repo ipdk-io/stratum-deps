bc0661d3cca5c7c8319b732c6594faf30814a0b4
Sabeel Ansari <sabeel.ansari@intel.com>
2023-11-10 06:05:05 -0800

    Update protobuf cmake to remove if block around abseil lib
    Signed-off-by: Sabeel Ansari <sabeel.ansari@intel.com>

--- cmake/protobuf.cmake
+++ cmake/protobuf.cmake
@@ -19,9 +19,7 @@ endif()
 # Protobuf v23.x bundles its own copy of Abseil.
 # We suppress it in favor of our own version or the
 # version bundles with gRPC.
-if(RECENT_PKGS)
-  set(_absl_provider -Dprotobuf_ABSL_PROVIDER=package)
-endif()
+set(_absl_provider -Dprotobuf_ABSL_PROVIDER=package)
 
 GetDownloadClause(_download_clause ${PROTOBUF_GIT_URL} ${PROTOBUF_GIT_TAG})
 
