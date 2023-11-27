9c125ffba1b9165557489faf58d894cc275b7cc0
Derek G Foster <derek.foster@intel.com>
2023-11-27 14:46:48 -0800

    Edit a couple of comments for clarity
    Signed-off-by: Derek G Foster <derek.foster@intel.com>

--- cmake/protobuf.cmake
+++ cmake/protobuf.cmake
@@ -10,14 +10,14 @@ unset(_source_subdir)
 
 # In older versions of protobuf, the primary CMakeLists.txt file
 # is in the cmake subdirectory.
-# Enabled by: { "SOURCE_SUBDIR": "cmake" }
+# Enabled by "defines": { "SOURCE_SUBDIR": "cmake" }
 if(DEFINED PROTOBUF_SOURCE_SUBDIR)
   set(_source_subdir SOURCE_SUBDIR ${PROTOBUF_SOURCE_SUBDIR})
 endif()
 
 # Protobuf v23.x generates unresolved external references to
 # ThreadSafeArena::ThreadCache _thread_cache if BUILD_SHARED_LIBS=ON.
-# Enabled by: { "BUILD_SHARED_LIBS": "OFF" }
+# Enabled by "defines": { "BUILD_SHARED_LIBS": "OFF" }
 if(DEFINED PROTOBUF_BUILD_SHARED_LIBS)
   set(_build_shared_libs ${PROTOBUF_BUILD_SHARED_LIBS})
 else()
@@ -26,7 +26,7 @@ endif()
 
 # Protobuf v23.x bundles its own copy of Abseil.
 # We suppress it in favor of our own version or the version bundled with gRPC.
-# Enabled by: { "ABSL_PROVIDER": "package" }
+# Enabled by "defines": { "ABSL_PROVIDER": "package" }
 if(DEFINED PROTOBUF_ABSL_PROVIDER)
   set(_absl_provider -Dprotobuf_ABSL_PROVIDER=${PROTOBUF_ABSL_PROVIDER})
 endif()
